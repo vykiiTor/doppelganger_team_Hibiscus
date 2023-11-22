5ff18442430325886c1acd23f7b4a92d7ecff13b
vykiiTor <vykiitor@gmail.com>
2023-11-22 10:56:30 +0100

    revert safe_calculator change

--- python/safe_calculator.py
+++ python/safe_calculator.py
@@ -5,6 +5,6 @@ class SafeCalculator:
     def add(self, left, right):
         authorized = self.authorizer.authorize()
         # Bug! Should be `if not authorized`
-        if not authorized:
+        if authorized:
             raise Exception("Not authorized")
         return left + right
