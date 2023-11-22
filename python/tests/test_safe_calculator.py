a6c934a2c4a9121d073311e1c25f613e4086bdbc
vykiiTor <vykiitor@gmail.com>
2023-11-22 11:02:23 +0100

    assert added

--- python/tests/test_safe_calculator.py
+++ python/tests/test_safe_calculator.py
@@ -1,16 +1,13 @@
 import pytest
 from safe_calculator import SafeCalculator
 
-class DummyAuthorizer:
-    def authorize(self):
-        return True
+
 
 def test_divide_should_not_raise_any_error_when_authorized():
     # TODO: write a test that fails due to the bug in SafeCalculator.add
     authorizer = Authorizer()
     safeCalculator = SafeCalculator(authorizer)
-    safeCalculator.add(1,2)
-    pass
+    assert(safeCalculator.add(1,2) == 3)
 
 class Authorizer:
 
