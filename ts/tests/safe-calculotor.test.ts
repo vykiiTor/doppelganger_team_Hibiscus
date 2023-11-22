d1256a81c4a00426dd803d8db88770dc0745b079
guiTheret <73123940+guiTheret@users.noreply.github.com>
2023-11-22 09:56:51 +0100

    up typescript test 1

--- ts/tests/safe-calculotor.test.ts
+++ ts/tests/safe-calculotor.test.ts
@@ -1,6 +1,9 @@
-import { test } from "@jest/globals"
+import { test, expect } from "@jest/globals";
 
-test('should not throw when authorized', () => {
+import { SafeCalculator } from "../safe-calculator";
+test("should not throw when authorized", () => {
   // TODO: write a test that fails due to the bug in
-  // SafeCalculator.add()
-})
+  const safeCalculator = new SafeCalculator({ authorize: () => false });
+  const result = safeCalculator.add(1, 2);
+  expect(result).toBe(new Error("Not authorized"));
+});
