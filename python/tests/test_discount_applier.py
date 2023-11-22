1925d013926b20136b8ffa931628f36a888b1bac
GitHermann <hermann.huang@efrei.net>
2023-11-22 10:51:51 +0100

    added test_discount_applier

--- python/tests/test_discount_applier.py
+++ python/tests/test_discount_applier.py
@@ -1,10 +1,42 @@
+import pytest
+from discount_applier import DiscountApplier
+
 def test_apply_v1():
     # TODO: write a test that fails due to the bug in
     # DiscountApplier.apply_v1
+    discount_applier, discount, users = init()
+
+    discount_applier.apply_v1(discount,users)
+    assert discount_applier.notifier.getNotifiedUsers() == users
+
     pass
 
 
 def test_apply_v2():
     # TODO: write a test that fails due to the bug in
     # DiscountApplier.apply_v2
+    discount_applier, discount, users = init()
+
+    discount_applier.apply_v2(discount,users)
+    assert discount_applier.notifier.getNotifiedUsers() == users
+
     pass
+
+def init():
+    discount_applier = DiscountApplier(Notifier())
+    discount = 50
+    users = ["Bob","Alice","Charlie"]
+
+    return discount_applier, discount, users
+
+
+class Notifier:
+    def __init__(self):
+        self.notifiedUsers = []
+
+    def notify(self,user,message):
+        self.notifiedUsers.append(user)
+        print(user+" "+message)
+    
+    def getNotifiedUsers(self):
+        return self.notifiedUsers
--- python/tests/test_safe_calculator.py
+++ python/tests/test_safe_calculator.py
@@ -1,7 +1,15 @@
 import pytest
+from safe_calculator import SafeCalculator
 
 
 def test_divide_should_not_raise_any_error_when_authorized():
-    # TODO: write a test that fails due to the bug in
-    # SafeCalculator.add
+    # TODO: write a test that fails due to the bug in SafeCalculator.add
+    authorizer = Authorizer()
+    safeCalculator = SafeCalculator(authorizer)
+    safeCalculator.add(1,2)
     pass
+
+class Authorizer:
+
+    def authorize(self):
+        return True
